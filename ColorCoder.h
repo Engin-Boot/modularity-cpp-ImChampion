87bef3de787416800ff3047862aab549a78372ea
Shubham Gaikwad <Shubham.gaikwad88.15@gmail.com>
2020-08-26 15:57:11 +0530

    ColorCoder

--- ColorCoder.h
+++ ColorCoder.h
@@ -27,15 +27,13 @@ namespace TelCoColorCoder
                 return majorColor;
             }
             MinorColor getMinor() {
-                return minorColor;
-            }
+                return minorColor; }
             std::string ToString() {
                 std::string colorPairStr = MajorColorNames[majorColor];
                 colorPairStr += " ";
                 colorPairStr += MinorColorNames[minorColor];
                 return colorPairStr;
-            }
-    };
+            }};
     ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = 
@@ -46,5 +44,4 @@ namespace TelCoColorCoder
     }
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
-    }
-    }
+    }  }
