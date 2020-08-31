737fe700c9a66abb94f588846cd2be45ea73d60f
Shubham <Shubham.gaikwad88.15@gmail.com>
2020-08-31 16:23:41 +0530

    Main Code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <assert.h>
 #include "ColorCoder.h"
-
 using namespace std;
-
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
@@ -14,15 +12,27 @@ void testNumberToPair(int pairNumber,
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
 }
-
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
+void testPairToNumber( TelCoColorCoder::MajorColor major,
     TelCoColorCoder::MinorColor minor,
     int expectedPairNumber)
+    {
+        int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+        std::cout << "Got pair number " << pairNumber << std::endl;
+        assert(pairNumber == expectedPairNumber);
+    }
+
+string ToString()
 {
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-    std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
+ string mapped_strings="";
+ int pairCount= TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::VIOLET,TelCoColorCoder::SLATE);
+ 
+    for(int pairNumber=1;pairNumber<=pairCount;pairNumber++)
+     {
+         TelCoColorCoder::ColorPair colorPair =
+            TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+         mapped_strings=mapped_strings + to_string(pairNumber)+"-->"+colorPair.ToString()+"\n";
+     }
+    return mapped_strings;
 }
 
 int main() {
@@ -31,5 +41,7 @@ int main() {
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    
+    cout<<ToString();
     return 0;
 }
